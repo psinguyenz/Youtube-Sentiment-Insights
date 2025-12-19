d18c1027366fb32dd13756e31c2e9464d6e16fed
Hoahong2002 <psinguyenz@gmail.com>
2025-12-19 15:12:35 +0700

    updated

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Youtube-Sentiment-Insights
+conda create -n youtube python=3.11 -y
+
+conda activate youtube
+
+pip install -r requirements.txt
+
+
+
+## DVC
+
+dvc init
+
+dvc repro
+
+dvc dag
+
+
+
+## AWS
+
+aws configure
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'youtube',
+    version = '0.0.0',
+    author = 'Siz',
+    author_email = 'psinguyenz@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+)
